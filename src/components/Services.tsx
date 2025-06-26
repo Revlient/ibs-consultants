8a1ee3bf8211d4148e02368adbcc62c7a6eb4352
Devaduth <kmdevaduth@gmail.com>
2025-06-26 15:19:21 +0530

    service card grid size changed

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -17,7 +17,7 @@ const Services: React.FC = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
           {services.map((service) => (
             <ServiceCard
               key={service.id}
