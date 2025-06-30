08ea12a7f465502a83473a5e975a8cbc1c0518f0
Devaduth <kmdevaduth@gmail.com>
2025-06-30 22:37:53 +0530

    Hero img

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero: React.FC = () => {
       <div 
         className="absolute inset-0 bg-cover bg-center z-0" 
         style={{ 
-          backgroundImage: "url('https://ibb.co/8nYRHgW8')", 
+          backgroundImage: "url('public/images/Hero.png')", 
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
